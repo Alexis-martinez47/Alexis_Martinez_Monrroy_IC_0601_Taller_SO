7765cb619b036000c665291f184a0a5ee020c466
Alexis-martinez47 <73048012+Alexis-martinez47@users.noreply.github.com>
2021-06-30 20:16:01 -0500

    Update Programa_Competencia 1.c

--- Desarrollo_1_competencia_1/Programa_Competencia 1.c
+++ Desarrollo_1_competencia_1/Programa_Competencia 1.c
@@ -8,52 +8,52 @@ char nombre[50];
 }Proceso;
 
 int main(int argc, char *argv[]){
-	int i,noProc,validacionTiempo;
-	printf("nombre de procesos: ");
-	scanf("%i",&noProc);
-	Proceso procesos[noProc];
-	for(i=0;i<noProc;i++){
-		printf("nombre de proceso ");
-		scanf("%s", procesos[i].nombre);
-		printf("Introduce el tiempo del proceso %i (milisegundos:)",i+1);
-		scanf("%i",&validacionTiempo);
-		procesos[i].tiempo=validacionTiempo;
-		if(valTiem=!0){
-			procesos[i].valido=1;
-		}else{
-			procesos[i].valido=0;
-		}
-	}
+int i,noProc,validacionTiempo;
+printf("nombre de procesos: ");
+scanf("%i",&noProc);
+Proceso procesos[noProc];
+for(i=0;i<noProc;i++){
+printf("nombre de proceso ");
+scanf("%s", procesos[i].nombre);
+printf("Introduce el tiempo del proceso %i (milisegundos:)",i+1);
+scanf("%i",&validacionTiempo);
+procesos[i].tiempo=validacionTiempo;
+if(valTiem=!0){
+procesos[i].valido=1;
+}else{
+procesos[i].valido=0;
+}
+}
 
-    int valQuantum;
-	printf("Introduce el valor del quantum: ");
-	scanf("%i",&valQuantum);
-	int procesosR=noProc,aux=0,vueltas=0;
-	while(procesosR!=0){
-		for(i=0;i<noProc;i++){
-			if(procesos[i].valido==1){
-				if(procesos[i].tiempo<=valQuantum){
-				procesos[i].tiempo=0;
-				procesos[i].valido=0;
-				printf("Proceso [%i]:[%s] quedo invalido\n",i+1,procesos[i].nombre);
-                }else{
-                    procesos[i].tiempo=procesos[i].tiempo-valQuantum;
+int valQuantum;
+printf("Introduce el valor del quantum: ");
+scanf("%i",&valQuantum);
+int procesosR=noProc,aux=0,vueltas=0;
+while(procesosR!=0){
+for(i=0;i<noProc;i++){
+if(procesos[i].valido==1){
+if(procesos[i].tiempo<=valQuantum){
+procesos[i].tiempo=0;
+procesos[i].valido=0;
+printf("Proceso [%i]:[%s] quedo invalido\n",i+1,procesos[i].nombre);
+ }else{
+ procesos[i].tiempo=procesos[i].tiempo-valQuantum;
 
-                }
-            }
-		}
-		vueltas++;
-        aux=0;
-        for(i=0;i<noProc;i++){
-            if(procesos[i].valido==0){
-                aux++;
-            }
-        }
-        if(aux==noProc){
-            procesosR=0;
-        }
+}
+}
+}
+vueltas++;
+  aux=0;
+  for(i=0;i<noProc;i++){
+  if(procesos[i].valido==0){
+  aux++;
+  }
+  }
+  if(aux==noProc){
+  procesosR=0;
+ }
 
-	}
-	printf("Vueltas totales: %i\n",vueltas);
-	return 0;
+}
+printf("Vueltas totales: %i\n",vueltas);
+return 0;
 }
